7c09bcc90129fc24ba93b63f42cea7aea3bcb9b3
Michael Meoak <mmeoak240@gmail.com>
2022-02-24 18:12:12 -0700

    commit tree 2f8d265bda1b5d2e5525dfe30961326d3d4fb7f0

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const num1 = 2000
+const num2 = 100
+
+const divide = function() {
+  return num1 / num2;
+}
+
+divide();
+
+const square = x => x * x;
+
+
+const add = (a, b) => a + b;
+
+add(3, 4);
+
